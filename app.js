f878e8f98f911a700c6d358e472c1360e532cd15
sakureatul <sakureatul@gmail.com>
2024-12-13 16:23:51 +0530

    this first react js code

---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+    "div",
+    {id:"parent"},[  React.createElement('div', {id:"child"},[
+        React.createElement('h1',{},"I am an H1 Tag"),
+        React.createElement('h2',{},"I am an H1 Tag")
+    ]),
+    React.createElement('div', {id:"child2"},[
+        React.createElement('h1',{},"I am an H1 Tag"),
+        React.createElement('h2',{},"I am an H1 Tag")
+    ])
+]
+  
+)
+console.log(parent);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
